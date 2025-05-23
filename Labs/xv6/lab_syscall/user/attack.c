b62c9a48a6e71f560722c0e7db68de737c49e0cd
Dongwei Xiang <davidxiang1997@gmail.com>
2025-05-23 10:11:31 +0800

    Add Labs/xv6/lab_syscall

---
+++ Labs/xv6/lab_syscall/user/attack.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+#include "kernel/riscv.h"
+
+int
+main(int argc, char *argv[])
+{
+  // your code here.  you should write the secret to fd 2 using write
+  // (e.g., write(2, secret, 8)
+  char *end = sbrk(17 * PGSIZE);
+  write(2, end + 16 * PGSIZE + 32, 8);
+  exit(1);
+}
